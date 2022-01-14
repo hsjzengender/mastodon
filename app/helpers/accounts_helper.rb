1e8d821d2ec48c6a3a273604a6b387c83fe23923
EqualMa <equalma@outlook.com>
2022-01-14 12:05:58 +0000

    hsjz: custom sign_in and sign_up page

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -103,6 +103,6 @@ module AccountsHelper
   end
 
   def svg_logo_full
-    content_tag(:svg, tag(:use, 'xlink:href' => '#mastodon-svg-logo-full'), 'viewBox' => '0 0 713.35878 175.8678')
+    content_tag(:svg, tag(:use, 'xlink:href' => '#mastodon-svg-logo-full'), 'viewBox' => '0 0 475 160')
   end
 end
